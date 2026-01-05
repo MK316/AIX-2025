67a0a4c0f9b16d2809854a430018afb643bcd446
MK <99416359+MK316@users.noreply.github.com>
2026-01-06 01:24:34 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.caption("AI-X Meeting")
+st.caption("AI-X Meeting (College of Education, GNU; 2025~2026)")
 
 st.markdown("📌 [AI-X Padlet link](https://padlet.com/mirankim316/AIX25)")
