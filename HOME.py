0a7308315dc55eca0953ae9a4176a73df22ba582
MK <99416359+MK316@users.noreply.github.com>
2026-01-06 01:42:42 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.caption("🍰 AI-X Meeting (College of Education, GNU; 2025~2026)")
 
-st.markdown("📌 [AI-X Padlet link](https://padlet.com/mirankim316/AIX25)")
+st.image(
+    "https://raw.githubusercontent.com/MK316/AIX-2025/main/aixbg.png",
+    use_container_width=True
+)
+
+
